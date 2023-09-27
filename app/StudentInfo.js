05f07347b9b9a6eddce112a4bc27257f05977702
ennis-ma <ennis@hustle.hk>
2023-09-26 22:41:37 -0600

    week3

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -1,17 +1,27 @@
 import React from "react";
 import Link from "next/link";
-import "./StudentInfo.css";
 
 const StudentInfo = () => {
   return (
-    <div className="student-info">
-      <p className="student-name">Name: Ennis</p>
-      <p className="course-section">Course Section: CPRG 306 A</p>
-      <p className="student-link">
-        <Link target="_blank" href="https://github.com/ennis-ma">
-          Github
-        </Link>
-      </p>
+    <div className="bg-gray-900 text-white p-8 rounded-xl shadow-lg max-w-md mx-auto mt-10">
+      <h2 className="text-2xl font-bold mb-4">Student Info</h2>
+      <div className="mb-4">
+        <label className="block text-sm font-medium mb-1">Name:</label>
+        <span className="text-lg">Ennis</span>
+      </div>
+      <div className="mb-4">
+        <label className="block text-sm font-medium mb-1">
+          Course Section:
+        </label>
+        <span className="text-lg">CPRG 306 A</span>
+      </div>
+      <Link
+        className="text-blue-500 hover:text-blue-400 hover:underline"
+        target="_blank"
+        href="https://github.com/ennis-ma"
+      >
+        Github
+      </Link>
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
-import React from "react";
 import Link from "next/link";
 import StudentInfo from "./StudentInfo";
 
 const Home = () => {
   return (
-    <>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+    <div className="bg-gray-900 text-white min-h-screen p-10">
+      <h1 className="text-3xl font-bold mb-6">
+        CPRG 306: Web Development 2 - Assignments
+      </h1>
       <StudentInfo />
-      <p>
-        <Link href="/week2">week2</Link>
+      <p className="mt-8 space-x-4">
+        <Link
+          className="text-blue-500 hover:text-blue-400 hover:underline"
+          target="_blank"
+          href="/week2"
+        >
+          week2
+        </Link>
+        <Link
+          className="text-blue-500 hover:text-blue-400 hover:underline"
+          target="_blank"
+          href="/week3"
+        >
+          week3
+        </Link>
       </p>
-    </>
+    </div>
   );
 };
 
